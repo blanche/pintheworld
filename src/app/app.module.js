34b6fb14db82ca03e57639c321715204cca93fc0
stellaxxiang <stellaxxiang@gmail.com>
2016-10-27 01:00:59 +0100

    Add AgmCoreModule and apikey

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { AgmCoreModule } from 'angular2-google-maps/core';
 
 let AppModule = NgModule({
     imports: [ BrowserModule,
+                AgmCoreModule.forRoot({
+                    apiKey: 'AIzaSyAgOrFWDMyp4XFYQRlY1a-cNxKNPz56ZQ4'
+                })
              ],
     declarations: [ AppComponent ],
     bootstrap: [ AppComponent ],
